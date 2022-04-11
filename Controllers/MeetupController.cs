7cf99b7e29ddf8a6c8a77655d9b0fbd2940f36ac
rovastall@gmail.com <rovastall@gmail.com>
2022-04-11 10:12:12 +0200

    Запрос GetAll

---
+++ Controllers/MeetupController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+
+namespace MeetupAPI.Controllers
+{
+    [Route("api/meetup")]
+    [ApiController]
+    public class MeetupController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+        public MeetupController(ILoggerManager logger, IRepositoryWrapper repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+        [HttpGet]
+        public IActionResult GetAllMeetups()
+        {
+            try
+            {
+                var meetups = _repository.Meetup.GetAllMeetups();
+                _logger.LogInfo($"Returned all meetups from database.");
+                return Ok(meetups);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllMeetups action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
